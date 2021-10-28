632df83fb2d5270aae4eb2998048f894135de454
kasfil <kasf@tuta.io>
2021-10-29 05:49:24 +0700

    adapt on cmp autopairs update

--- lua/lsp/completion.lua
+++ lua/lsp/completion.lua
@@ -52,9 +52,7 @@ cmp.setup({
     },
 })
 
+-- autopairs
 require("nvim-autopairs").setup()
-require("nvim-autopairs.completion.cmp").setup({
-    map_cr = true, --  map <CR> on insert mode
-    map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-    auto_select = false, -- automatically select the first item
-})
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
