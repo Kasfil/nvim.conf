1f99a16c9a7c761423cdcd5e1069ac7999f0dba6
kasfil <kasf@tuta.io>
2021-10-31 01:39:08 +0700

    adapt nvim-tree update

--- lua/packs/nvimtree.lua
+++ lua/packs/nvimtree.lua
@@ -1,10 +1,8 @@
 local g = vim.g
 
-g.nvim_tree_ignore = {".git", "node_modules", "cache", "__pycache__"}
 g.nvim_tree_gitignore = 1
 g.nvim_tree_quit_on_open = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 0
 
@@ -12,6 +10,10 @@ require("nvim-tree").setup({
     view = {
         width = 32,
         side = "left",
+    },
+    filters = {
+        dotfiles = true,
+        custom = {".git", "node_modules", "cache", "__pycache__"}
     },
     auto_close = true,
     disable_netrw = true,
