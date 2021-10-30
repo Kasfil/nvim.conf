452b05a614e076cf4859ea48fc4b4fd801b9e1c5
kasfil <kasf@tuta.io>
2021-10-31 01:39:37 +0700

    update nvim-cmp mapping config

--- lua/lsp/completion.lua
+++ lua/lsp/completion.lua
@@ -45,10 +45,14 @@ cmp.setup({
             end
         end, { "i", "s" }),
 
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
 })
 
