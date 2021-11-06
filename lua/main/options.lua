0013eaae0560d3642fd0f948c2a2b89e81aad912
kasfil <kasf@tuta.io>
2021-11-06 18:53:13 +0700

    clean main.init into options

--- lua/main/options.lua
+++ lua/main/options.lua
@@ -1,29 +1,25 @@
 local opt = vim.opt
+local g = vim.g
 
+-- Neovim/Vim options
 opt.termguicolors = true
 opt.mouse = "nv"
-opt.errorbells = true
-opt.visualbell = true
 opt.hidden = true
 opt.fileformats = "unix,mac,dos"
-opt.magic = true
-opt.virtualedit = "block"
-opt.encoding = "utf-8"
-opt.viewoptions = "folds,cursor,curdir,slash,unix"
-opt.sessionoptions = "curdir,help,tabpages,winsize"
 opt.clipboard = "unnamedplus"
 opt.wildignorecase = true
-opt.wildignore = ".git,.hg,.svn,*.pyc,*.o,*.out,*.jpg,*.jpeg,*.png,*.gif,*.zip,**/tmp/**,*.DS_Store,**/node_modules/**,**/bower_modules/**"
+opt.wildignore = {
+    -- files
+    ".git", ".hg", ".svn", "*.pyc", "*.o", "*.out", "*.DS_Store",
+    -- Directories
+    "**/tmp/**", "**/node_modules/**", "**/bower_modules/**", "**/__pycache__/**"
+}
 opt.backup = false
 opt.writebackup = false
 opt.swapfile = false
 opt.history = 2000
 opt.shada = "!,'3000,<50,@100,s10,h"
-opt.backupskip = "/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,*/shm/*,/private/var/*,.vault.vim"
-opt.smarttab = true
 opt.shiftround = true
-opt.timeout = true
-opt.ttimeout = true
 opt.timeoutlen = 500
 opt.ttimeoutlen = 10
 opt.updatetime = 100
@@ -46,21 +42,17 @@ opt.switchbuf = "useopen"
 opt.backspace = "indent,eol,start"
 opt.diffopt = "filler,iwhite,internal,algorithm:patience"
 opt.completeopt = "menuone,noselect"
-opt.jumpoptions = "stack"
 opt.showmode = false
 opt.shortmess = "aoOTIcF"
 opt.scrolloff = 2
 opt.sidescrolloff = 5
 opt.foldlevelstart = 99
 opt.ruler = false
-opt.list = true
-opt.winwidth = 30
-opt.winminwidth = 10
 opt.pumheight = 15
 opt.helpheight = 12
 opt.previewheight = 12
 opt.showcmd = false
-opt.cmdheight = 2
+opt.cmdheight = 1
 opt.cmdwinheight = 5
 opt.laststatus = 2
 opt.display = "lastline"
@@ -82,6 +74,25 @@ opt.signcolumn = "yes"
 opt.conceallevel = 2
 opt.colorcolumn = "89,101"
 
-if vim.g.nvui then
-    opt.guifont = "JetBrains Mono:h10.5"
-end
+-- Disable distributed plugin
+g.loaded_gzip = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_2html_plugin = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
+g.loaded_netrwFileHandlers = 1
+
+-- Set map leader
+g.mapleader = ","
