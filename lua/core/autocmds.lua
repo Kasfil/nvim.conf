87a0e921d1a27b528d33b53c06f64abe1837ad1e
kasfil <kasf@tuta.io>
2022-02-13 20:14:35 +0700

    autocmds for fugitive buffer

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,3 +1,4 @@
 -- close dap-float filetypes with q
 vim.cmd [[autocmd FileType dap-float nnoremap <buffer><silent> q <cmd>close!<CR>]]
 vim.cmd [[autocmd FileType qf nnoremap <buffer><silent> q <cmd>close!<CR>]]
+vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent> q <cmd>close!<CR>]]
