72105e7541813c0e3f531e62748d41afcf2b8f1b
kasfil <kasf@tuta.io>
2022-02-17 07:39:01 +0700

    update autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -4,6 +4,7 @@ vim.cmd [[autocmd FileType qf nnoremap <buffer><silent> q <cmd>close!<CR>]]
 
 -- fugitive buffer mapping
 vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent> q <cmd>close!<CR>]]
-vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> gPp <cmd>Dispatch! git push<CR>]]
+vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> gPp <cmd>Git! -c push.default=current push<CR>]]
 vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> gpp <cmd>Dispatch git fetch<CR>]]
 vim.cmd [[autocmd FileType gitcommit nnoremap <buffer><silent> q <cmd>close!<CR>]]
+vim.cmd [[autocmd FileType git nnoremap <buffer><silent> q <cmd>close!<CR>]]
