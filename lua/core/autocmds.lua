0c5098125d53431084a89b94ae352ac0733518fd
kasfil <kasf@tuta.io>
2022-02-21 19:01:06 +0700

    add yanked highlight and update fugitive buffer mapping

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -4,7 +4,16 @@ vim.cmd [[autocmd FileType qf nnoremap <buffer><silent> q <cmd>close!<CR>]]
 
 -- fugitive buffer mapping
 vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent> q <cmd>close!<CR>]]
-vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> gPp <cmd>Dispatch! git -c push.default=current push<CR>]]
-vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> gpp <cmd>Dispatch git fetch<CR>]]
+vim.cmd [[autocmd FileType fugitive nnoremap <buffer> co<space> :Git switch ]]
+vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> cP<space> <cmd>Dispatch! git -c push.default=current push<CR>]]
+vim.cmd [[autocmd FileType fugitive nnoremap <buffer><silent><nowait> cp<space> <cmd>Dispatch git fetch<CR>]]
 vim.cmd [[autocmd FileType gitcommit nnoremap <buffer><silent> q <cmd>close!<CR>]]
 vim.cmd [[autocmd FileType git nnoremap <buffer><silent> q <cmd>close!<CR>]]
+
+-- Highlight yanked
+vim.cmd [[
+    augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=700})
+    augroup END
+]]
