914a80839cada2af907a3ec54aa3ab4ec2d634cb
kasfil <kasf@tuta.io>
2022-03-10 22:12:57 +0700

    api: update to new autocmd and augroup api (v0.7)

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,14 +1,24 @@
--- close dap-float filetypes with q
-vim.cmd [[autocmd FileType dap-float nnoremap <buffer><silent> q <cmd>close!<CR>]]
-vim.cmd [[autocmd FileType qf nnoremap <buffer><silent> q <cmd>close!<CR>]]
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- close window with q
+autocmd("FileType", {
+    pattern = {"dap-float", "qf"},
+    command = "nnoremap <buffer><silent> q <cmd>close!<CR>"
+})
 
 -- Highlight yanked
-vim.cmd [[
-    augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=700})
-    augroup END
-]]
+local hi_yanked = augroup("HighlightYanked", {clear = true})
+autocmd("TextYankPost", {
+    group = hi_yanked,
+    callback = function()
+        vim.highlight.on_yank({higroup = "IncSearch", timeout = 500})
+    end
+})
 
 -- nvim lightbulb
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
+autocmd({"CursorHold", "CursorHoldI"}, {
+    callback = function()
+        require("nvim-lightbulb").update_lightbulb()
+    end
+})
