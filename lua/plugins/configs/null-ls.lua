a4eb8b74e2fa324728734b257e43c916087aa119
kasfil <kasf@tuta.io>
2022-05-09 07:56:07 +0700

    simplify null-ls config

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -1,55 +1,6 @@
 local null_ls = require("null-ls")
 local b = null_ls.builtins
 
-local h = require("null-ls.helpers")
-local methods = require("null-ls.methods")
-local u = require("null-ls.utils")
-
-local severities = {
-    error = vim.lsp.protocol.DiagnosticSeverity.Error,
-    warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
-}
-
-local single_revive = h.make_builtin({
-    name = "revive",
-    method = methods.internal.DIAGNOSTICS_ON_SAVE,
-    filetypes = { "go" },
-    generator_opts = {
-        command = "revive",
-        to_stdin = true,
-        from_stderr = false,
-        ignore_stderr = false,
-        args = {
-            "-formatter",
-            "json",
-            "$FILENAME",
-        },
-        format = "json",
-        multiple_files = false,
-        check_exit_code = function(code)
-            return code == 0
-        end,
-        on_output = function(params)
-            local diags = {}
-            for _, d in ipairs(params.output) do
-                local filename = u.path.join(params.root, d.Position.Start.Filename)
-                table.insert(diags, {
-                    row = d.Position.Start.Line,
-                    col = d.Position.Start.Column,
-                    end_row = d.Position.End.Line,
-                    end_col = d.Position.End.Column,
-                    source = "revive",
-                    message = d.Failure,
-                    severity = severities[d.Severity],
-                    filename = filename,
-                })
-            end
-            return diags
-        end,
-    },
-    factory = h.generator_factory,
-})
-
 local sources = {
     -- python
     b.diagnostics.flake8,
@@ -58,7 +9,9 @@ local sources = {
 
     -- golang
     b.formatting.goimports,
-    single_revive,
+    b.diagnostics.revive.with({
+        args = { "-formatter", "json", "$FILENAME" }
+    }),
 
     -- rust
     b.formatting.rustfmt,
@@ -68,7 +21,6 @@ local sources = {
 }
 
 null_ls.setup({
-    debug = true,
     sources = sources,
 
     -- format on save
