2c677775c61dda5300d509710e8d43e9ed800e4b
kasfil <kasf@tuta.io>
2022-06-16 09:42:48 +0700

    update nvim-test window to split instead of float

--- lua/plugins/configs/nvim-test.lua
+++ lua/plugins/configs/nvim-test.lua
@@ -1,8 +1,8 @@
 require("nvim-test").setup({
     term = "toggleterm",
     termOpts = {
-        direction = "float",
-        width = 180,
+        direction = "vertical",
+        width = 102,
         height = 45,
     },
 })
