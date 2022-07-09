0361f3062a480807174a849e4f77b7f95340a806
kasfil <kasf@tuta.io>
2022-07-09 20:29:23 +0700

    format and add nvim-surround highlight

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -7,69 +7,72 @@ local colors = {
 
 local custom_hl = {
     -- normal
-    NonText = {fg = palette.sumiInk3},
-    CursorLine = {bg = palette.sumiInk2},
-    WinSeparator = {fg = palette.sumiInk4},
-    SideWin = {fg = palette.fujiWhite, bg = palette.sumiInk0},
+    NonText = { fg = palette.sumiInk3 },
+    CursorLine = { bg = palette.sumiInk2 },
+    WinSeparator = { fg = palette.sumiInk4 },
+    SideWin = { fg = palette.fujiWhite, bg = palette.sumiInk0 },
     -- LineNr = {bg = palette.sumiInk0},
 
     -- HTML (mostly linked by markdown)
-    htmlBoldItalic = {fg = palette.oniViolet, italic = true, bold = true},
+    htmlBoldItalic = { fg = palette.oniViolet, italic = true, bold = true },
 
     -- virtcolumn
-    VirtColumn = {fg = palette.sumiInk2},
+    VirtColumn = { fg = palette.sumiInk2 },
 
     -- nvim-tree
-    NvimTreeNormal = {link = "SideWin"},
-    NvimTreeNormalNC = {link = "SideWin"},
+    NvimTreeNormal = { link = "SideWin" },
+    NvimTreeNormalNC = { link = "SideWin" },
 
     -- treesitter
     -- Comment = {fg = palette.fujiGray, italic = true},
 
     -- Telescope
-    TelescopePromptNormal = {bg = palette.autumnGreen, fg = palette.sumiInk3},
-    TelescopePromptBorder = {bg = palette.autumnGreen, fg = palette.autumnGreen},
-    TelescopePromptTitle = {bg = palette.autumnYellow, fg = palette.sumiInk3},
-    TelescopePromptPrefix = {fg = palette.sumiInk3, bg = palette.autumnGreen},
-    TelescopePromptCounter = {fg = palette.sumiInk3},
+    TelescopePromptNormal = { bg = palette.autumnGreen, fg = palette.sumiInk3 },
+    TelescopePromptBorder = { bg = palette.autumnGreen, fg = palette.autumnGreen },
+    TelescopePromptTitle = { bg = palette.autumnYellow, fg = palette.sumiInk3 },
+    TelescopePromptPrefix = { fg = palette.sumiInk3, bg = palette.autumnGreen },
+    TelescopePromptCounter = { fg = palette.sumiInk3 },
 
-    TelescopeResultsNormal = {bg = palette.sumiInk2},
-    TelescopeResultsBorder = {bg = palette.sumiInk2, fg = palette.sumiInk2},
-    TelescopeResultsTitle = {bg = palette.sumiInk2, fg = palette.sumiInk2},
+    TelescopeResultsNormal = { bg = palette.sumiInk2 },
+    TelescopeResultsBorder = { bg = palette.sumiInk2, fg = palette.sumiInk2 },
+    TelescopeResultsTitle = { bg = palette.sumiInk2, fg = palette.sumiInk2 },
 
-    TelescopePreviewNormal = {bg = palette.sumiInk0},
-    TelescopePreviewBorder = {bg = palette.sumiInk0, fg = palette.sumiInk0},
-    TelescopePreviewTitle = {bg = palette.carpYellow, fg = palette.sumiInk0},
+    TelescopePreviewNormal = { bg = palette.sumiInk0 },
+    TelescopePreviewBorder = { bg = palette.sumiInk0, fg = palette.sumiInk0 },
+    TelescopePreviewTitle = { bg = palette.carpYellow, fg = palette.sumiInk0 },
 
-    TelescopeSelection = {bg = palette.sumiInk3},
-    TelescopeSelectionCaret = {fg = palette.waveRed, bg = palette.sumiInk3},
+    TelescopeSelection = { bg = palette.sumiInk3 },
+    TelescopeSelectionCaret = { fg = palette.waveRed, bg = palette.sumiInk3 },
 
     -- Lightspeed
-    LeapBackdrop = {fg = palette.fujiGray},
+    LeapBackdrop = { fg = palette.fujiGray },
 
     -- SymbolsOutline
-    FocusedSymbol = {bg = "NONE"},
+    FocusedSymbol = { bg = "NONE" },
 
     -- dap nvim
-    DebugPC = {bg = palette.winterRed},
-    DapStatusLine = {bg = "NONE", fg = palette.roninYellow, bold = true},
+    DebugPC = { bg = palette.winterRed },
+    DapStatusLine = { bg = "NONE", fg = palette.roninYellow, bold = true },
 
     -- Gitsigns
-    GitSignsCurrentLineBlame = {fg = palette.waveBlue2},
+    GitSignsCurrentLineBlame = { fg = palette.waveBlue2 },
 
     -- neogit
-    NeogitNotificationInfo = {fg = palette.springBlue},
-    NeogitNotificationWarning = {fg = palette.carpYellow},
-    NeogitNotificationError = {fg = palette.peachRed},
+    NeogitNotificationInfo = { fg = palette.springBlue },
+    NeogitNotificationWarning = { fg = palette.carpYellow },
+    NeogitNotificationError = { fg = palette.peachRed },
 
     -- bqf
-    BqfPreviewBorder = {fg = palette.springGreen, bg = palette.sumiInk0},
-    BqfPreviewFloat = {bg = palette.sumiInk0},
+    BqfPreviewBorder = { fg = palette.springGreen, bg = palette.sumiInk0 },
+    BqfPreviewFloat = { bg = palette.sumiInk0 },
 
     -- dressing
-    DressingInput = {bg = palette.sumiInk1, fg = palette.fujiWhite},
-    DressingBorder = {bg = palette.sumiInk1, fg = palette.autumnYellow},
-    FloatTitle = {bg = palette.sumiInk1, fg = palette.autumnYellow, bold = true, italic = true},
+    DressingInput = { bg = palette.sumiInk1, fg = palette.fujiWhite },
+    DressingBorder = { bg = palette.sumiInk1, fg = palette.autumnYellow },
+    FloatTitle = { bg = palette.sumiInk1, fg = palette.autumnYellow, bold = true, italic = true },
+
+    -- nvim-surround
+    NvimSurroundHighlightTextObject = { link = "IncSearch" },
 }
 
 require("kanagawa").setup({
