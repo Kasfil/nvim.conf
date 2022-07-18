e33a23985718467239d720a3bcf1a70c5513f2aa
kasfil <kasf@tuta.io>
2022-07-18 08:25:20 +0700

    add sql as requirerd file

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -2,7 +2,7 @@ local treesitter = require("nvim-treesitter.configs")
 local ensured = {
     "go", "gomod", "rust", "comment", "javascript", "typescript",
     "python", "graphql", "json", "jsonc", "lua", "yaml", "toml", "vue",
-    "markdown", "markdown_inline"
+    "markdown", "markdown_inline", "sql",
 }
 
 treesitter.setup({
