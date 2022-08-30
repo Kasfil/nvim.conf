eb5fe3235ceb21f2af5071db5da7611a589d76f4
kasfil <kasf@tuta.io>
2022-08-31 00:01:22 +0700

    fix: update go.nvim config

--- lua/user/configs/gonvim.lua
+++ lua/user/configs/gonvim.lua
@@ -2,5 +2,10 @@ import("go", function(gonvim)
   gonvim.setup({
     icons = false,
     dap_debug_keymap = false,
+    comment_placeholder = ": ",
+    lsp_keymaps = false,
+    lsp_inlay_hints = {
+      enable = true,
+    },
   })
 end)
