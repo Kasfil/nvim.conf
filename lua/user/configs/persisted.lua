1e64f7f8a94b7b31bea3f2cdfbab07cfa0aba84e
kasfil <kasf@tuta.io>
2022-10-29 19:37:59 +0700

    feat: adopt persisted.nvim deprecation message

--- lua/user/configs/persisted.lua
+++ lua/user/configs/persisted.lua
@@ -1,16 +1,21 @@
-require("persisted").setup({
+import("persisted", function(persisted)
+  persisted.setup({
     autoload = true,
     autosave = true,
+    branch_separator = "_",
     before_save = function()
-        local cmds = {
-            "NvimTreeClose",
-            "SymbolsOutlineClose"
-        }
+      local cmds = {
+        "NvimTreeClose",
+        "SymbolsOutlineClose",
+      }
 
-        for _, cmd in ipairs(cmds) do
-            vim.notify("running " .. cmd, "warning")
-            -- prevent error
-            pcall(function() vim.cmd(cmd) end)
-        end
-    end
-})
+      for _, cmd in ipairs(cmds) do
+        vim.notify("running " .. cmd, "warning")
+        -- prevent error
+        pcall(function()
+          vim.cmd(cmd)
+        end)
+      end
+    end,
+  })
+end)
